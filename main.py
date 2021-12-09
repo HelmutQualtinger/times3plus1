91e294d20e6f64ef1a3bb938c7132eb93c4584fe
Harald Beker <haraldbeker@MacBook-Air-von-Harald.local>
2021-12-09 23:55:51 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+def times_three_plus_one(start):
+    result = []
+    while (start > 1):
+        result.append(start)
+        if (start % 2) == 1:
+            start = 3 * start + 1
+        else:
+            start /= 2
+        start = int(start)
+    return result
+
+
+s=[]
+lenList=[]
+maxValList=[]
+maxValIndList=[]
+for i in range(2, 100000):
+    series = times_three_plus_one(i)
+    s.append(i)
+    ll = len(series)  # länge der Serie bis 1 erreicht
+    m = max(series)  # maximal erreichter wert
+    ind = series.index(m)  # nach wie vielen Iterationen
+    lenList.append(ll)
+    maxValList.append(m)
+    maxValIndList.append(ind)
+#    print('%5d %3d %5d %5d' % (i, ind, ll, m))
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10,7),dpi=100)
+plt.title("Time 3 plus 1 Problem")
+plt.xlabel("Starting value")
+plt.ylabel("Maximum value reached")
+plt.plot(maxIndList,maxValList,'b.')
+plt.plot(s,lenList,"r-")
+plt.plot(y_scondary="Number of iterations")
+plt.yscale("log")
+plt.xscale("linear")
+plt.show()
